b2572ca96fdb074edd8cc3885149c9bc2c3ac82f
John R. D'Orazio <priest@johnromanodorazio.com>
2022-01-27 21:55:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+include_once( 'includes/enums/AcceptHeader.php' );
+include_once( 'includes/enums/RequestMethod.php' );
+include_once( 'includes/enums/RequestContentType.php' );
 include_once('includes/AlexaSkill.php');
 
 $AlexaSkill = new AlexaSkill();
+$AlexaSkill->APICore->setAllowedRequestMethods( [ RequestMethod::GET, RequestMethod::POST, RequestMethod::OPTIONS ] );
+$AlexaSkill->APICore->setAllowedRequestContentTypes( [ RequestContentType::JSON, RequestContentType::FORMDATA ] );
+$AlexaSkill->APICore->setAllowedAcceptHeaders( [ AcceptHeader::JSON ] );
